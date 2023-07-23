9232468042fa7d675e7ff6a7cfc6a2a731d535a1
Nitin Gupta <nitin.gupta.22@ucl.ac.uk>
2023-07-23 16:26:31 +0100

    remove extra file

--- run.py
+++ run.py
@@ -11,44 +11,40 @@ logging.getLogger('scapy.runtime').setLevel(logging.ERROR)
 
 
 # Directory containing pcap files to be processed
-def main():
-    zeroShotModels = ZeroShotModels()
-    excelOperations = ExcelOperations()
-    pcap_operations = PcapOperations()
-    directory = './inputs'
-    argParser = argparse.ArgumentParser()
 
-    argParser.add_argument("-t", "--token", help="Huggingface Auth Token")
-    argParser.add_argument("-m", "--model", help="Model Name")
-    argParser.add_argument("-s", "--suffix", help="Model Suffix")
-    argParser.add_argument("-d", "--directory", help="Directory containing pcap files to be processed")
-    args = argParser.parse_args()
+zeroShotModels = ZeroShotModels()
+excelOperations = ExcelOperations()
+pcap_operations = PcapOperations()
+directory = './inputs'
+argParser = argparse.ArgumentParser()
 
-    if args.directory:
-        directory = args.directory
+argParser.add_argument("-t", "--token", help="Huggingface Auth Token")
+argParser.add_argument("-m", "--model", help="Model Name")
+argParser.add_argument("-s", "--suffix", help="Model Suffix")
+argParser.add_argument("-d", "--directory", help="Directory containing pcap files to be processed")
+args = argParser.parse_args()
 
-    if args.token:
-        os.environ['HF_TOKEN'] = args.token
+if args.directory:
+    directory = args.directory
 
-    if args.model:
-        zero_shot_models = zeroShotModels.get_models_by_name(args.model)
-    elif args.suffix:
-        zero_shot_models = zeroShotModels.get_models_by_suffix(args.suffix)
-    else:
-        zero_shot_models = zeroShotModels.get_all_models()
+if args.token:
+    os.environ['HF_TOKEN'] = args.token
 
-    print(f"Processing {len(zero_shot_models)} models")
+if args.model:
+    zero_shot_models = zeroShotModels.get_models_by_name(args.model)
+elif args.suffix:
+    zero_shot_models = zeroShotModels.get_models_by_suffix(args.suffix)
+else:
+    zero_shot_models = zeroShotModels.get_all_models()
 
-    finalData = []
+print(f"Processing {len(zero_shot_models)} models")
 
-    for zero_shot in zero_shot_models:
-        zero_shot["base_truth"] = excelOperations.read_xlsx()
-        pcap_operations.process_files(zero_shot, directory)
-        finalData.append(zero_shot["model_output"])
-        del zero_shot
+finalData = []
 
-    excelOperations.create_excel_file(finalData)
+for zero_shot in zero_shot_models:
+    zero_shot["base_truth"] = excelOperations.read_xlsx()
+    pcap_operations.process_files(zero_shot, directory)
+    finalData.append(zero_shot["model_output"])
+    del zero_shot
 
-
-if __name__ == "__main__":
-    main()
+excelOperations.create_excel_file(finalData)
