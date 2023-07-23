4726018978a424204f375aa45f974ad516b69e18
Nitin Gupta <nitin.gupta.22@ucl.ac.uk>
2023-07-24 00:09:12 +0100

    zero-shot maker

--- zero-shot-model-maker.py
+++ zero-shot-model-maker.py
@@ -17,8 +17,6 @@ candidate_labels = [
 model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
 model.config.bos_token_id = 1
 model.config.eos_token_id = 2
-model = model.eval()
-model = torch.compile(model)
 
 
 label2id = {label: i for i, label in enumerate(candidate_labels)}
@@ -28,5 +26,8 @@ model.config.label2id = label2id
 
 # Convert the model to zero-shot mode
 model.config.zero_shot_classification = True
+
+model = model.eval()
+model = torch.compile(model)
 
 model.push_to_hub("niting3c/llama-2-7b-hf-zero-shot", use_auth_token=True)
