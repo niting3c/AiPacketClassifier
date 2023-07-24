131aa6581c9979e9d9838fe349233dae2f895f76
Nitin Gupta <nitin.gupta.22@ucl.ac.uk>
2023-07-24 01:27:11 +0100

    zero-shot maker

--- zero-shot-model-maker.py
+++ zero-shot-model-maker.py
@@ -1,9 +1,9 @@
-import transformers
 import torch
+import transformers
 
 tokenizer = transformers.LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
 
-model = transformers.LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+model = transformers.AutoModelForSequenceClassification.from_pretrained("meta-llama/Llama-2-7b-hf")
 
 ZERO_SHOT = "zero-shot-classification"
 ATTACK = "attack"
@@ -14,16 +14,16 @@ candidate_labels = [
     NORMAL
 ]
 
+model.config.architectures = ["LlamaForSequenceClassification", ""]
 model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
 model.config.bos_token_id = 1
 model.config.eos_token_id = 2
 
-
 label2id = {label: i for i, label in enumerate(candidate_labels)}
-
+id2label = {i: label for i, label in enumerate(candidate_labels)}
 # Set the model's label mapping
 model.config.label2id = label2id
-
+model.config.id2label = id2label
 # Convert the model to zero-shot mode
 model.config.zero_shot_classification = True
 
