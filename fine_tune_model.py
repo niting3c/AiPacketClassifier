0847a904e817d931651e829fc682bfef0c8f88eb
Nitin Gupta <nitin.gupta.22@ucl.ac.uk>
2023-08-01 10:52:03 +0100

    fine tuner

--- fine_tune_model.py
+++ fine_tune_model.py
@@ -1,37 +1,29 @@
 import datasets
-import torch
-from datasets import load_dataset, ClassLabel, Features, Value
-from transformers import LlamaTokenizer, TrainingArguments, Trainer, \
-    DataCollatorForTokenClassification, AutoModelForSequenceClassification
-
-model = AutoModelForSequenceClassification.from_pretrained("meta-llama/Llama-2-7b-hf")
-tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
-tokenizer.add_special_tokens({'pad_token': "-100"})
-
-candidate_labels = [
-    "attack",
-    "normal"
-]
-model_features = Features({'text': Value('string'), 'label': ClassLabel(names=candidate_labels)})
-OUTPUT_DIR = "test_trainer"
+from datasets import load_dataset
+from transformers import LlamaTokenizer, TrainingArguments, Trainer, AutoModelForSequenceClassification, \
+    DataCollatorForTokenClassification
 
 
 def get_training_args():
-    return TrainingArguments(output_dir=OUTPUT_DIR,
-                             logging_strategy="epoch",
-                             label_names=candidate_labels,
-                             load_best_model_at_end=True,
-                             evaluation_strategy="steps",
-                             eval_steps=50,
-                             warmup_steps=100,
-                             save_steps=50,
-                             num_train_epochs=10,
-                             do_train=True,
-                             do_eval=True,
-                             )
+    candidate_labels = ["attack", "normal"]
+    return TrainingArguments(
+        output_dir="test_trainer",
+        logging_strategy="epoch",
+        label_names=candidate_labels,
+        load_best_model_at_end=True,
+        evaluation_strategy="steps",
+        eval_steps=50,
+        warmup_steps=100,
+        save_steps=50,
+        num_train_epochs=10,
+        do_train=True,
+        do_eval=True,
+    )
 
 
 def get_data_set(from_percent, to_percent, filename, seed=42):
+    model_features = datasets.Features(
+        {'text': datasets.Value('string'), 'label': datasets.ClassLabel(names=candidate_labels)})
     return load_dataset("niting3c/malicious-packet-analysis",
                         features=model_features,
                         data_files=filename,
@@ -44,50 +36,81 @@ def get_data_set(from_percent, to_percent, filename, seed=42):
 
 
 def tokenize_function(examples):
-    return tokenizer(examples["text"], padding=False, truncation=False,
-                     max_length=3600)
+    return tokenizer(examples["text"], padding=True, truncation=True, max_length=3600)
 
 
+model = AutoModelForSequenceClassification.from_pretrained("meta-llama/Llama-2-7b-hf")
+tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+tokenizer.add_special_tokens({'pad_token': "-100"})
+
+# Define candidate labels and model config settings
+candidate_labels = ["attack", "normal"]
+model.config.architectures = ["LlamaForSequenceClassification"]
+model.config.zero_shot_classification = True
+
+# Prepare datasets
 normal_dataset_0_90_train = get_data_set(0, 90, "data/normal.csv")
-
 normal_dataset_validate = get_data_set(90, 100, "data/normal.csv")
-
 mixed_dataset_0_90_train = get_data_set(0, 90, "data/mixed_data.csv")
-
 mixed_dataset_validate = get_data_set(90, 100, "data/mixed_data.csv")
 
+# Prepare training arguments and data collator
 training_args = get_training_args()
-
 data_collator = DataCollatorForTokenClassification(
     tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
 )
 
-model.config.use_cache = False
-model.config.architectures = ["LlamaForSequenceClassification", ""]
-model.config.zero_shot_classification = True
+# Train and evaluate on the normal dataset
+try:
+    trainer = Trainer(
+        model=model,
+        train_dataset=normal_dataset_0_90_train,
+        eval_dataset=normal_dataset_validate,
+        args=training_args,
+        data_collator=data_collator,
+    )
 
-trainer = Trainer(
-    model=model,
-    train_dataset=normal_dataset_0_90_train,
-    eval_dataset=normal_dataset_validate,
-    args=get_training_args(),
-    data_collator=data_collator,
-)
+    trainer.train()
+    trainer.save_model()  # Save the model after training.
 
-trainer.train()
-trainer.evaluate()
+    # Evaluate and report evaluation results.
+    eval_results = trainer.evaluate()
+    print("Evaluation results on normal dataset:", eval_results)
 
-trainer = Trainer(
-    model=model,
-    train_dataset=mixed_dataset_0_90_train,
-    eval_dataset=mixed_dataset_validate,
-    args=get_training_args(),
-    data_collator=data_collator,
-)
+except Exception as e:
+    print("An error occurred during training on normal dataset:")
+    print(str(e))
+    exit(1)
 
-model.save_pretrained(OUTPUT_DIR)
-model = torch.compile(model)
-model.push_to_hub("niting3c/llama-2-7b-hf-zero-shot", use_auth_token=True)
+# Train and evaluate on the mixed dataset
+try:
+    trainer = Trainer(
+        model=model,
+        train_dataset=mixed_dataset_0_90_train,
+        eval_dataset=mixed_dataset_validate,
+        args=training_args,
+        data_collator=data_collator,
+    )
 
-tokenizer.save_pretrained(OUTPUT_DIR)
-tokenizer.push_to_hub("niting3c/llama-2-7b-hf-zero-shot", use_auth_token=True)
+    trainer.train()
+    eval_results_mixed = trainer.evaluate()
+    print("Mixed data evaluation results:", eval_results_mixed)
+
+except Exception as e:
+    print("An error occurred during training on mixed dataset:")
+    print(str(e))
+    exit(1)
+
+# Save the model and tokenizer to Hugging Face Hub.
+model.save_pretrained("test_trainer")
+tokenizer.save_pretrained("test_trainer")
+
+# Push to Hugging Face Hub.
+try:
+    model.push_to_hub("niting3c/llama-2-7b-hf-zero-shot", use_auth_token=True)
+    tokenizer.push_to_hub("niting3c/llama-2-7b-hf-zero-shot", use_auth_token=True)
+    print("Model and tokenizer pushed to the Hugging Face Hub.")
+except Exception as e:
+    print("An error occurred while pushing to the Hugging Face Hub:")
+    print(str(e))
+    exit(1)
