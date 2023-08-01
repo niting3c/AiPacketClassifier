8d5d0abc766e6fdeb6e3113163d1145d4bed581a
Nitin Gupta <nitin.gupta.22@ucl.ac.uk>
2023-08-01 04:20:47 +0100

    fine tuner

--- fine_tune_model.py
+++ fine_tune_model.py
@@ -1,10 +1,12 @@
 import datasets
 import torch
 from datasets import load_dataset, ClassLabel, Features, Value
-from transformers import AutoTokenizer, TrainingArguments, Trainer, \
-    LlamaForSequenceClassification, DataCollatorForTokenClassification
+from transformers import LlamaTokenizer, TrainingArguments, Trainer, \
+    LlamaForSequenceClassification, DataCollatorForTokenClassification,AutoModelForSequenceClassification
 
 from models import ZeroShotModels
+model = AutoModelForSequenceClassification.from_pretrained("meta-llama/Llama-2-7b-hf")
+tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
 
 candidate_labels = [
     "attack",
@@ -12,7 +14,6 @@ candidate_labels = [
 ]
 model_features = Features({'text': Value('string'), 'label': ClassLabel(names=candidate_labels)})
 OUTPUT_DIR = "test_trainer"
-
 
 def get_training_args():
     return TrainingArguments(output_dir=OUTPUT_DIR,
@@ -41,23 +42,9 @@ def get_data_set(from_percent, to_percent, filename, seed=42):
                         ).map(tokenize_function, batched=True).shuffle(seed=seed)
 
 
-zero_shot = ZeroShotModels()
-model_entry = zero_shot.get_models_by_suffix("llama-2-7b")[0]
-tokenizer = AutoTokenizer.from_pretrained(model_entry["model_name"])
-
-
-def generate_prompt(data_point):
-    return f"""
-### Input:
-{data_point["input"]}
-### Response:
-{data_point["output"]}
-"""
-
-
 def tokenize_function(examples):
     return tokenizer(examples["text"], padding=False, truncation=True,
-                       max_length=model_entry["context_size"])
+                       max_length=3600)
 
 
 normal_dataset_0_90_train = get_data_set(0, 90, "data/normal.csv")
@@ -68,19 +55,18 @@ mixed_dataset_0_90_train = get_data_set(0, 90, "data/mixed_data.csv")
 
 mixed_dataset_validate = get_data_set(90, 100, "data/mixed_data.csv")
 
-model_entry["model"] = LlamaForSequenceClassification.from_pretrained(model_entry["model_name"], num_labels=2)
 training_args = get_training_args()
 
 data_collator = DataCollatorForTokenClassification(
     tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
 )
 
-model_entry["model"].config.use_cache = False
-model_entry["model"].config.architectures = ["LlamaForSequenceClassification", ""]
-model_entry["model"].config.zero_shot_classification = True
+model.config.use_cache = False
+model.config.architectures = ["LlamaForSequenceClassification", ""]
+model.config.zero_shot_classification = True
 
 trainer = Trainer(
-    model=model_entry["model"],
+    model=model,
     train_dataset=normal_dataset_0_90_train,
     eval_dataset=normal_dataset_validate,
     args=get_training_args(),
@@ -91,15 +77,15 @@ trainer.train()
 trainer.evaluate()
 
 trainer = Trainer(
-    model=model_entry["model"],
+    model=model,
     train_dataset=mixed_dataset_0_90_train,
     eval_dataset=mixed_dataset_validate,
     args=get_training_args(),
     data_collator=data_collator,
 )
 
-model_entry["model"].save_pretrained(OUTPUT_DIR)
-model = torch.compile(model_entry["model"])
+model.save_pretrained(OUTPUT_DIR)
+model = torch.compile(model)
 model.push_to_hub("niting3c/llama-2-7b-hf-zero-shot", use_auth_token=True)
 
 tokenizer.save_pretrained(OUTPUT_DIR)
